7b7424b886a919aca917a5eae4c144ad12685b63
theanishtar <dangtt135@gmail.com>
2024-11-08 10:38:47 +0700

    init

---
+++ src/my-vr-tour.js
@@ -0,0 +1,6 @@
+// Hot spot creation function
+function hotspot(hotSpotDiv, args) {
+  console.log(hotSpotDiv);
+  console.log(args);
+  // hotSpotDiv.innerHTML += "<iframe width='300' height='150' src='https://www.youtube.com/embed/EIA_1rbxyQA?si=aPEu-3Xnrqes5MeT' title='YouTube video player' frameborder='0' allow='accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share' allowfullscreen></iframe>";
+}
