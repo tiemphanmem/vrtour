efd49d2d1d10aaac363864606430f619ab9566ab
theanishtar <dangtt135@gmail.com>
2024-12-11 11:48:35 +0700

    add meeting-room 5n3

---
+++ index.js
@@ -0,0 +1,28 @@
+const http = require('http');
+const fs = require('fs');
+const path = require('path');
+
+const hostname = 'localhost';
+const port = 3000;
+
+const server = http.createServer((req, res) => {
+  // Đường dẫn tới tệp HTML
+  const filePath = path.join(__dirname, 'index.html');
+
+  // Đọc tệp HTML
+  fs.readFile(filePath, (err, data) => {
+    if (err) {
+      res.statusCode = 500;
+      res.setHeader('Content-Type', 'text/plain');
+      res.end('Internal Server Error');
+    } else {
+      res.statusCode = 200;
+      res.setHeader('Content-Type', 'text/html');
+      res.end(data);
+    }
+  });
+});
+
+server.listen(port, hostname, () => {
+  console.log(`Server running at http://${hostname}:${port}/`);
+});
